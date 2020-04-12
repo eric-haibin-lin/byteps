96018118989bfba9936f0e888948394a0fde8b73
Ubuntu <ubuntu@ip-172-31-6-181.ec2.internal>
2020-04-12 02:01:03 +0000

    move location

--- example/mxnet/dist_server.sh
+++ example/mxnet/dist_server.sh
@@ -1,9 +1,9 @@
 # now you are in docker environment
 export BYTEPS_FORCE_DISTRIBUTED=1
 export DMLC_NUM_WORKER=1
-export DMLC_ROLE=scheduler
+export DMLC_ROLE=server
 export DMLC_NUM_SERVER=1
-export DMLC_PS_ROOT_URI=172.31.35.7
+export DMLC_PS_ROOT_URI=172.31.6.181
 export DMLC_PS_ROOT_PORT=12345
 
 python3 /home/ubuntu/byteps/launcher/launch.py
--- example/mxnet/dist_worker.sh
+++ example/mxnet/dist_worker.sh
@@ -5,7 +5,7 @@ export DMLC_NUM_WORKER=1
 export DMLC_ROLE=worker
 export DMLC_NUM_SERVER=1
 export DMLC_WORKER_ID=0
-export DMLC_PS_ROOT_URI=172.31.35.7
+export DMLC_PS_ROOT_URI=172.31.10.108
 export DMLC_PS_ROOT_PORT=12345
 
 python3 /home/ubuntu/byteps/launcher/launch.py  python3 ~/byteps/example/mxnet/train_gluon_mnist_byteps.py
